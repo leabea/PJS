f603c319ce01f832aa4920cf037bd28a997d25da
leabea <lea.karimi95@gmail.com>
2022-06-28 23:44:46 +0200

    enter

--- imputationMean.py
+++ imputationMean.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
-%matplotlib inline 
 
-#CSV-Datei auslesen
-dataInput = pd.read_csv('data.csv') 
+
+
+# CSV-Datei auslesen
+dataInput = pd.read_csv('data.csv')
 
 print(type(dataInput))
 
@@ -12,16 +13,16 @@ print(dataInput)
 
 print(dataInput.columns)
 
-#Plotten der Verteilung
+# Plotten der Verteilung
 dataInput.hist('LSTAT')
 
 dataInput.boxplot('LSTAT', showmeans=True)
 
-#Imputation fehlender Werte in allen Spalten durch den Mittelwert der jeweiligen Spalte
-dataInput=dataInput.fillna(dataInput.mean())
+# Imputation fehlender Werte in allen Spalten durch den Mittelwert der jeweiligen Spalte
+dataInput = dataInput.fillna(dataInput.mean())
 
-#Alle Stellen im Dataframe auf 2 Nachkommastellen runden
-dataInput=dataInput.round(2)
+# Alle Stellen im Dataframe auf 2 Nachkommastellen runden
+dataInput = dataInput.round(2)
 
-#neue CSV Datei erstellen
+# neue CSV Datei erstellen
 dataInput.to_csv('dataNewMittelwert.csv')
